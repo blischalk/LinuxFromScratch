d132029a216362e1899d86d15bfc85f7af024971
Brett Lischalk <blischalk@brettlischalk.com>
2020-10-29 21:13:47 -0500

    Acquire sources

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/bash
 echo "Building LFS"
+export LFS=/lfs
+echo "export LFS=$LFS" > /root/.bash_profile
+echo "export LFS=$LFS" > /root/.bashrc
+mkdir -v $LFS/sources
+chmod -v a+wt $LFS/sources
+wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
+pushd $LFS/sources
+  md5sum -c md5sums
+popd
+
